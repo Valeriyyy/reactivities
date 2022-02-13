2325cbb6a0e6374dcb1d18e275f32a913b9fab16
Valeriyyy <valeriykutsar18@gmail.com>
2022-02-12 20:50:56 -0800

    added cors

--- API/Startup.cs
+++ API/Startup.cs
@@ -21,6 +21,11 @@ namespace API
 			services.AddDbContext<DataContext>(options => {
 				options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
 			});
+			services.AddCors(opt => {
+				opt.AddPolicy("CorsPolicy", policy => {
+					policy.AllowAnyMethod().AllowAnyHeader().AllowAnyOrigin().WithOrigins("http://localhost:3000");
+				});
+			});
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -34,6 +39,8 @@ namespace API
 			app.UseHttpsRedirection();
 
 			app.UseRouting();
+
+			app.UseCors("CorsPolicy");
 
 			app.UseAuthorization();
 
